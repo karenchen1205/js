2616ebfbae3381450cacbd229b49f4d6ff85e30f
karenchen1205 <72909888+karenchen1205@users.noreply.github.com>
2022-12-05 23:38:15 +0800

    Create acronym.js

---
+++ acronym.js
@@ -0,0 +1,6 @@
+var word1 = 'random';
+var word2 = 'access';
+var word3 = 'memory';
+var acronym = ['random','access','memory']
+var el = document.getElementById('acronym');
+el.textContent = acronym[0][0].toUpperCase()+acronym[1][0].toUpperCase()+acronym[2][0].toUpperCase();
